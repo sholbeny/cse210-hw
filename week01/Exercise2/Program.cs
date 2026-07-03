f835b3144f4764df416c9f41b923c6afb40a8348
sholbeny <sholbeny@byupathway.edu>
2026-07-03 14:27:26 -0500

    exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Diagnostics.Contracts;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (grade >=70)
+        {
+            Console.WriteLine("Congratulations! You pass!");
+        }
+        else
+        {
+            Console.WriteLine("Not quite passing, but keep going!");
+        }
     }
 }
