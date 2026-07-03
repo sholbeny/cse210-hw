a5150ad13558906351490647446c2693f3ec588d
sholbeny <sholbeny@byupathway.edu>
2026-07-03 14:59:21 -0500

    exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Parts 1 and 2:
+        // Console.WriteLine("What is the Magic Number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+        
+        
+        while (guess != magicNumber)
+        
+        {
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+                }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+                }
+
+        Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+        
+        }
+        Console.WriteLine("Congratulations! You got it correct!");
     }
 }
